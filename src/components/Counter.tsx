6595c667ad6f443b6d57f894b0557e6d1bf8d4f0
phy-lei <765373325@qq.com>
2024-04-19 11:04:28 +0800

    feat: 更新用例

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,28 +1,27 @@
-import { defineCustomElement as defineTaroButtonCore } from '@tarojs/components/dist/components/taro-button-core';
+import { View, Text, Button } from '@tarojs/components'
 import { createSignal } from 'solid-js'
 import './counter.css'
-defineTaroButtonCore()
+
 export default function Counter(props) {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(props.count)
 
   const add = () => {
     setCount(count() + 1)
-    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', count());
   }
 
   const sub = () => {
     setCount(count() - 1)
   }
 
-
-
   return (
     <>
-      <taro-view-core class="flex">
-        <taro-button-core onClick={add}>+</taro-button-core>
-        <taro-text-core class="add">{count()}</taro-text-core>
-        <taro-button-core onClick={sub}>-</taro-button-core>
-      </taro-view-core>
+      <View class="flex">
+        <Button onClick={add}>+</Button>
+        <Text class="add">{count()}
+          <Text>{Math.random()}</Text>
+        </Text>
+        <Button onClick={sub}>-</Button>
+      </View>
     </>
   )
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,30 +1,29 @@
-import { defineCustomElement as defineTaroButtonCore } from '@tarojs/components/dist/components/taro-button-core';
-import { useLoad } from "@tarojs/taro";
-import Counter from "@/components/Counter";
-import { createSignal } from "solid-js";
-import "./index.css";
-defineTaroButtonCore()
+import { View, Text, Button } from '@tarojs/components'
+import Counter from '@/components/Counter'
+import { createSignal, onMount } from 'solid-js'
+import './index.css'
 
 export default function Index() {
-  const [color, setColor] = createSignal("red");
-  const [count, setCount] = createSignal(0)
-  useLoad(() => {
-    console.log(`[ _createElement('input') ] >`,)
-  });
+  const [color, setColor] = createSignal('red')
+  const [cls, setCls] = createSignal('')
+  let divRef
 
-
+  onMount(() => {
+    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', divRef);
+  })
 
   return (
-    <taro-view-core class="index">
-      <taro-view-core style={`color: ${color()}`} >
-        <taro-text-core>{count()}</taro-text-core>
-      </taro-view-core>
-      <taro-button-core  onClick={() => setColor('blue')}>set color</taro-button-core >
-      <taro-button-core onClick={() => setCount(count() +1)}>add</taro-button-core>
-      <taro-view-core>
-        {Math.random()}
-      </taro-view-core>
-      <Counter></Counter>
-    </taro-view-core>
-  );
+    <View class="index">
+      <View data-name="uid" style={{color: color(), margin: '10px'}} classList={{ bold: color() === "blue" }}>
+        <Text ref={divRef}>Hello world! </Text>
+      </View>
+      <View style={`color: ${color()}`} id={color()}>
+        <Text class={cls()}>Hello world2! </Text>
+      </View>
+      <Button onClick={() => setCls('bold')}>set class</Button>
+      <Button onClick={() => setColor('blue')}>set style</Button>
+      <Counter count={0}></Counter>
+      <View>{Math.random()}</View>
+    </View>
+  )
 }
