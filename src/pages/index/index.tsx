34ee07b0ee960c798604d66146bab27184958cfc
phy-lei <765373325@qq.com>
2024-04-19 16:08:58 +0800

    update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,16 @@
 import { View, Text, Button } from '@tarojs/components'
 import Counter from '@/components/Counter'
-import { createSignal, onMount } from 'solid-js'
+import { createSignal, onMount, createMemo} from 'solid-js'
 import './index.css'
 
+
+const colorMap = ["red", "green", "blue"];
 export default function Index() {
-  const [color, setColor] = createSignal('red')
+  const [color, setColor] = createSignal(0)
   const [cls, setCls] = createSignal('')
   let divRef
+
+  const currentColor = createMemo(() => colorMap[color() % 3])
 
   onMount(() => {
     console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', divRef);
@@ -14,14 +18,14 @@ export default function Index() {
 
   return (
     <View class="index">
-      <View data-name="uid" style={{color: color(), margin: '10px'}} classList={{ bold: color() === "blue" }}>
+      <View data-name="uid" class="flex" style={{color: currentColor(), margin: '10px'}} classList={{ bold: currentColor() === "blue" }}>
         <Text ref={divRef}>Hello world! </Text>
       </View>
-      <View style={`color: ${color()}`} id={color()}>
+      <View style={`color: ${currentColor()}`} id={currentColor()}>
         <Text class={cls()}>Hello world2! </Text>
       </View>
       <Button onClick={() => setCls('bold')}>set class</Button>
-      <Button onClick={() => setColor('blue')}>set style</Button>
+      <Button onClick={() => setColor(color() + 1)}>set style</Button>
       <Counter count={0}></Counter>
       <View>{Math.random()}</View>
     </View>
